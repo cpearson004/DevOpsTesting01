82a07b40b2c89311280b65a53a6b36acc5b32a92
Ubuntu <ubuntu@ip-172-31-63-239.ec2.internal>
2021-01-29 15:53:35 +0000

    test

---
+++ terraform-scripts/creds.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+
+  access_key = "ASIA5BWBIQ5P3J7HEH4L" 
+
+  secret_key = "QBEYAixiM31IIn0AjW9MXPlSryLmHjNqsH7cEW0/" 
+
+  token = "FwoGZXIvYXdzEKH//////////wEaDPuYlex9vkbWABjwGyK/AUZPB3oRoCUYeaiDaiXHSZE8/ZjLc7yn+Xny1If+kY9zJCVreuUif8Sk0sEgYwc3J2D2IrEPBvjOeqJA6tLLbW+gHYtSnALgP5nItlJZA3im6LrE3jcH5sIm6fI0gKgS6s2k3pxtG8UH+VZkaa2xZeOOuNh0XvyqXXhiVNwsHSX3DWmh4IDTuFiua0gigStiR1WCFKzfih0/5XUwyZ0YpcqU2hk3SShVZ5YwFnKN/hp9dMiSEsQDfkV6RuGPDwTTKLnQ0IAGMi3woV2/nq2bk4cm/22dYf//vNmEtz3Rgl9HUvgBgHWP1YIVS5F6GiXAR0UZyTk="
+
+  region = "us-east-1"
+
+}
+
